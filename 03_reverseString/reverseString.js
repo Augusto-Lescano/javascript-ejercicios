55d0aaac79e2660df9009ff654cbf0cde32a1568
Augusto Lescano <augustolescano15@gmail.com>
2025-03-25 07:03:47 -0300

    Se resolvió el ejercicio 2 de los ejercicios de javascript y se pasaron todas las pruebas de desarrollo

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let normal=str.split('')
+  let reverse=new Array(str.length)
+  let reverseIndex=str.length-1
+  for(let i=0;i<str.length;i++){
+    reverse[i]=normal[reverseIndex]
+    reverseIndex--
+  }
+  return reverse.join('')
 };
+
+console.log(reverseString('hello'))
 
 // Do not edit below this line
 module.exports = reverseString;
