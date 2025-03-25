9a18a669188a6483c2e9032552522451f2ef32f4
Augusto Lescano <augustolescano15@gmail.com>
2025-03-25 16:25:26 -0300

    Se resuelve el ejercicio 6 y se pasan todas las pruebas de desarrollo

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(anio) {
+  if(anio%4===0){
+    if(anio%100 !== 0){
+      return true
+    }else if(anio%400===0){
+      return true
+    }else{
+      return false
+    }
+  }else{
+    return false
+  }
 };
 
 // Do not edit below this line
