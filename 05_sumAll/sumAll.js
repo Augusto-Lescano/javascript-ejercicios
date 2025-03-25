2981dc4a345ce08fbe050869e599f6ccaaa4ce58
Augusto Lescano <augustolescano15@gmail.com>
2025-03-25 16:01:26 -0300

    Se resuelve ejercicio 5 y se pasan todas las pruebas de desarrollo

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(n1,n2) {
+  let suma=0
+  if(n1>=0 && n2>=0){
+    if(Number.isInteger(n1)===true && Number.isInteger(n2)===true){
+      if(n1>n2){
+        let cambio
+        cambio=n2
+        n2=n1
+        n1=cambio
+      }
+      for(let i=n1;i<=n2;i++){
+        suma+=i
+      }
+      return suma
+    }else{
+      return 'ERROR'  
+    }
+  }else{
+    return 'ERROR'
+  }
 };
 
 // Do not edit below this line
