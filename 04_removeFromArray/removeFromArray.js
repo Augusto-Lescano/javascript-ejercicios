7df9bf05879a7d784fc410847306af520c38f018
Augusto Lescano <augustolescano15@gmail.com>
2025-03-25 15:38:52 -0300

    Se resuelve el ejercicio 3 y se pasan todas las pruebas de desarrollo

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...borrar) {
+  let nuevoArr=[]
+  let indice=0
+  for(let i=0;i<arr.length;i++){
+    let prueba=0
+    for(let j=0;j<borrar.length;j++){
+      if(arr[i]===borrar[j]){
+        prueba+=1
+      }
+    }
+    if(prueba===0){
+      nuevoArr[indice]=arr[i]
+      indice+=1
+    }
+  }
+  return nuevoArr
 };
+
+console.log(removeFromArray([1,2,3,4],2,3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
